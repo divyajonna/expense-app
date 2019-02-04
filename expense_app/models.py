dcdafe3c70f6c896b451c973a4e57b02e7704fa7
DivyaJonna <divya.jonna@invensis.net>
2019-02-04 10:27:00 +0530

    models and superuser created

--- expense_app/models.py
+++ expense_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.utils import timezone
+from datetime import datetime
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+class Expense(models.Model):
+	client = models.ForeignKey(Client,null=True,on_delete=models.CASCADE)
+	title = models.CharField(max_length=100,null=True)
+	currency = models.CharField(max_length=100,null=True,blank=True)
+	amount = models.CharField(max_length=100,null=True,blank=True)
+	timestampOfExpense = models.DateTimeField(default=datetime.now,null=True)
+	description = models.TextField(max_length=300,null=True,blank=True)
+
+	def __unicode__(self):
+		return self.title
